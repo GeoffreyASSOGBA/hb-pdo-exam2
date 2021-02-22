21486390c40c9fb7ca8a0f434e75d4d4eab3124f
geoffrey <biggeo22@hotmail.fr>
2021-02-22 22:52:05 +0100

    Exo 4

--- 4-insert-prepare.php
+++ 4-insert-prepare.php
@@ -2,6 +2,7 @@
 
 include 'includes/connect.php';
 
+
 $data = [
     [
         'composer' => 'Joseph-François Garnier',
@@ -29,3 +30,25 @@ $data = [
         'year'     => 1762,
     ],
 ];
+
+// préparer la requête
+$sqlMusic = "INSERT INTO music (name, year, composer_id) VALUES (:name, :year, :composer_id)";
+$statement = $connection->prepare($sqlMusic);
+if ($statement === false) {
+    exit("Erreur lors de l'insertion de la donnée :");
+}
+foreach ($data as $music) {
+    $composerName = $music['composer'];
+    $sqlComposer = "SELECT id FROM composer WHERE name LIKE '$composerName'";
+    // exécuter la requête pour le composer et récupérer le champ id
+    $rep = $connection->query($sqlComposer);
+    $donnees = $rep->fetch();
+
+    echo $donnees['id'];
+    // Utiliser la méthode bindParam pour donner des valeurs aux 3 champs
+    $statement->bindParam(':name', $music['name'], PDO::PARAM_STR);
+    $statement->bindParam(':year', $music['year'], PDO::PARAM_INT);
+    $statement->bindParam(':composer_id', $donnees['id'], PDO::PARAM_INT);
+    // exécuter la requête pour la music
+    $statement->execute();
+}
