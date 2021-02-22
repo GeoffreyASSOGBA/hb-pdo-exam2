ca104e922ba1c86de6e426bc9deef5faf49647f8
geoffrey <biggeo22@hotmail.fr>
2021-02-22 13:30:45 +0100

    "Fin de la connexion"

--- includes/connect.php
+++ includes/connect.php
@@ -2,3 +2,12 @@
 
 include 'db.inc.php';
 
+$dsn = 'mysql:dbname=geoffreyexampdo;host=127.0.0.1';
+$user = 'root'; // Utilisateur par défaut
+$password = ''; // Par défaut, pas de mot de passe sur Wamp
+
+try {
+    $connection = new PDO($dsn, $user, $password);
+} catch (PDOException $e) {
+    echo 'Connexion échouée : ' . $e->getMessage();
+}
